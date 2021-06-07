344f798a27e6c35beda692a0f2d6d7d82dfb83de
mattyruffinel <71448981+mattyruffinel@users.noreply.github.com>
2021-06-07 16:04:57 -0400

    exercice 1

--- exercice.py
+++ exercice.py
@@ -4,12 +4,23 @@
 PERCENTAGE_TO_LETTER = {"A*": [95, 101], "A": [90, 95], "B+": [85, 90], "B": [80, 85], "C+": [75, 80], "C": [70, 75], "F": [0, 70]}
 
 # TODO: Importez vos modules ici
+import json
+import csv
 
 
 # TODO: Définissez vos fonction ici
+def compare_fichier(path1=str,path2=str):
+    fichier1=open(path2)
+    fichier2=open(path1)
+    for thing in fichier1:
+        if thing not in fichier2:
+            print(f"les fichiers sont differents a {thing} ")
+            break
+        else:
+            print('les fichiers sont identiques')
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-
+    compare_fichier('exemple.txt','notes.txt')
     pass
